03dc030a7758d58d88bd6252571ea1fed705724f
Ghost-Rog <ghostrog@mail.ru>
2020-12-27 22:19:28 +0300

    Initial commit

---
+++ src/main/java/ru/ghostrog/mvc/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package ru.ghostrog.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String HelloPage(){
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodByePage(){
+        return "first/goodbye";
+
+    }
+}
